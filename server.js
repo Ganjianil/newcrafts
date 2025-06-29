0bb7b6344692833bbea53841231978a2148920ee
rocky <ganjianil@outlook.com>
2025-06-29 20:05:25 +0530

    main

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 10406;
+const port = 10145;
 const mysql = require("mysql2");
 const multer = require("multer");
 const upload = multer({ dest: 'upload/' });
